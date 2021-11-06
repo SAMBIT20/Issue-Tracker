71a48151cdb8b3608e5c8078345f1b0bcc295c94
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2021-11-06 10:31:19 +0530

    mogodb Cloud added

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,9 @@ const development = {
   name: "development",
   PORT: 8000,
   asset_path: "public",
-  mongodb_URI: "mongodb://localhost/issue-tracker",
+  //mongodb_URI: "mongodb://localhost/issue-tracker",
+  mongodb_URI:
+    "mongodb+srv://sambitn20:TY8y5UIKeOnA3eZd@cluster0.rwhme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
 
 const environment = development;
